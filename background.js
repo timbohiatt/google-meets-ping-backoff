c97f5f529293a654347727f6c0ad0f70071b294e
Tim Hiatt <timhiatt@google.com>
2022-08-25 22:14:59 +0000

    manifest fix

--- background.js
+++ background.js
@@ -9,18 +9,18 @@ const watchedURLs = [
 ];
 
 
+function logNotifications(data) {
+  const ids = Object.keys(data);
+  console.log("Logging Notifications:")
+  console.log(data)
+  console.log(ids)
+}
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // Check the Watched URLs
   for (let i = 0; i < watchedURLs.length; i++) {
     if (tab.url.includes(watchedURLs[i])) {
-
-      chrome.notifications.getAll(
-        (data => {
-          console.log("Logging Notifications:")
-          console.log(data)
-        }
-        )
-      )
+      chrome.notifications.getAll().then(logNotifications)
     }
   }
 
